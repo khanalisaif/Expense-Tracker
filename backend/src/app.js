4e934581a840bff8781ddecca8b3318e6b4c9d93
Saif <hasansaifkhan@gmail.com>
2026-03-08 18:55:25 +0530

    a

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(cors({ origin: "https://expense-tracker-topaz-tau.vercel.app/", credentials: true }));
+app.use(cors({ origin: "https://expense-tracker-topaz-tau.vercel.app", credentials: true }));
 app.use(express.json());
 
 app.use("/api/expenses", expenseRoutes);
